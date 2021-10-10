2689a535b69b27006234ca5bbc8347bfd8e3e4f2
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-10-10 15:27:24 -0300

    feat: config sources projets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="app">
       <h1>Olá mundo</h1>
+
+      <GlobalStyle />
     </div>
   );
 }
